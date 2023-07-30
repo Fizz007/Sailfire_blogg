d8c4c6f81920b33f65fae9f200a63702d4e1e417
Faisal Abrar <erfaisalabrar@gmail.com>
2023-07-30 22:28:50 +0530

    Sailfire blog

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["images.pexels.com"],
-  },
+  // images: {
+  //   domains: ["images.pexels.com"],
+  // },
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
